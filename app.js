5eec4d5c61c84bf34b12edf6b4ff9c7c0757168f
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-05-18 10:30:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const header = {
     algorithm: process.env.HEADER_ALG
 }
 
+const token = jwt.sign(payload, process.env.SECRET, header);
+
+
+
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
