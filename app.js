97d30646ad4ec928fd1df358ba6200a465c4b0dc
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-05-18 10:17:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const db = mongoose.connection;
 
 
@@ -10,6 +11,17 @@ app.use(express.static('public'))
 require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//APi token
+const payload = {
+    userId: user._id, 
+    role: "costumer",
+    exp: (Date.now() / 1000) + (60 * 60)
+}
+
+const header = {
+    algorithm: process.env.HEADER_ALG
+}
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', console.error.bind(console, 'connection error:'));
