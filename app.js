86249890551d3cff09cd50ce2b09fc0e7f3176fd
Tommy Danielsson <tdserviceco@gmail.com>
2021-05-20 10:40:01 +0200

    routes added and cleanup done, begin testing phase

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const {register, auth, products, orders} = require('./functions/routes')
-const { createToken, deCryptHash, hashGenerator, verifyToken } = require('./functions/methods');
+const { register, auth, products, orders, allProducts, allOrders, productById, removeProduct } = require('./functions/routes')
 const db = mongoose.connection;
-
-
 
 //Middleware
 app.use(express.static('public'))
@@ -16,152 +12,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 const connection = () => {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function () {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
 
+        //Routes 
+        /** GET ROUTES */
+        app.get('/api/products/:id', productById)
+        app.get('/api/products', allProducts)
+        app.get('/api/orders', allOrders)
+        /** POST ROUTES */
+        app.post('/api/products', products)
+        app.post('/api/register', register)
+        app.post('/api/auth', auth)
+        app.post('/api/orders', orders)
 
-    //Routes 
-   
-    // for register user 
-    app.post('/api/register', register) 
-     
+        /** DELETE ROUTES */
+        app.delete('/api/products/:id', removeProduct)
 
-      
-    
-    //for sign in
+        /** PATCH ROUTES */
+        app.patch('/api/products/:id', updateProduct)
 
-    app.post('/api/auth', auth)
-    /* async (req, res) => {
-    
-        const user = await User.findOne({ 
-          email: req.body.email
-        });
-        if(user && deCryptHash(req.body.password, user.password)) {
-            const token = createToken(user);
-            res.cookie('auth-token', token);
-            tokenInCookies = token;
-            res.json({msg: "You're logged in!"});
-        } else {
-            res.json({msg: "Login failed. Invalid credentials."});
-        }
-    });
- */
-    // add product 
-    //only access for admins
-    app.post('/api/products', products)
-    
-    
-    /* (req, res) => {
-    
-        const token = req.cookies["auth-token"];
-    
-        if (token === tokenInCookies){
-            const userPayload = verifyToken(token);
-            if (userPayload.role == 'Admin') {
-                const newProduct = new Product({
-                    title: req.body.title,
-                    price: req.body.price,
-                    shortDesc: req.body.shortDesc,
-                    longDesc: req.body.longDesc,
-                    imgFile: req.body.imgFile,
-                    })
-        
-                    newProduct.save((err) => {
-                    err ? res.send(err) : res.json(newProduct)
-                    });
-                } else {
-                    res.json({msg: 'Unauthorized'});
-                }
-        } else {
-            res.json({msg: 'Please, log in'})
-        }
-    }) */
-
-    //get all products
-    app.get('/api/products', async (req, res) => {
-      const allProducts = await Product.find({})
-      res.json(allProducts)
-    });
-
-    //get product byId
-    app.get('/api/products/:id', async (req, res) => {
-      const getProduct = await Product.findById(req.params.id)
-      res.json(getProduct)
     })
-
-    //delete product byId
-    app.delete('/api/products/:id', async (req, res) => {
-        const token = req.cookies["auth-token"];
-    
-        if (token === tokenInCookies){
-            const userPayload = verifyToken(token);
-            if (userPayload.role == 'Admin') {
-                const removeProduct = await Product.deleteOne({ _id: req.params.id });
-                res.json(removeProduct);
-            } else {
-                res.json({msg: 'Unauthorized'})
-            }
-        } else {
-            res.json({msg: "Please, log in"});
-        }
-    })
-
-    //Update Product
-    //insert function for valdition 
-    app.patch('/api/products/:id', async (req, res) => {
-        const token = req.cookies["auth-token"];
-    
-        if (token === tokenInCookies){
-            const userPayload = verifyToken(token);
-            if (userPayload.role == 'Admin') {
-                const updateProduct = await Product.updateOne({ _id: req.params.id }, { $set: { price: req.body.price } })
-                res.json(updateProduct);
-            } else {
-                res.json({msg: 'Unauthorized'})
-            }
-        } else {
-            res.json({msg: "Please, log in"});
-        }
-    })
-
-    //post orders
-    app.post('/api/orders', orders)
-    /* (req, res) => {
-        const token = req.cookies["auth-token"];
-    
-        if (token === tokenInCookies){
-            const userPayload = verifyToken(token);
-
-            const postOrder = new Order({
-                        status: req.body.status,
-                        items: req.body.items,
-                        buyer: userPayload.userId,
-                        orderValue: req.body.items.length
-            });
-
-            postOrder.save((err) => {
-                if (err) console.error(err);
-                res.json(postOrder);
-            });
-
-        } else {
-            res.json({msg: "Please, log in"});
-        }
-    })
- */
-    //get all Orders
-    app.get('/api/orders', async (req, res) => {
-      const allOrders = await Order.find({})
-      res.json(allOrders)
-    })
-  })
 }
-
-
-
-
 module.exports = { app, connection }
--- functions/routes.js
+++ functions/routes.js
@@ -1,30 +1,30 @@
 let tokenInCookies = '';
-
+const admin = 'admin';
 const User = require('../models/user')
 const Product = require('../models/product')
 const Order = require('../models/order')
+const jwt = require('jsonwebtoken');
 const { createToken, deCryptHash, hashGenerator, verifyToken } = require('./methods');
 
 const register = async (req, res, next) => {
-  const user = await User.findOne({email: req.body.email});
+  const user = await User.findOne({ email: req.body.email });
 
-      if(!user){
-            console.log(req.body);
-            const newUser = new User({
-            email: req.body.email,
-            name: req.body.name,
-            password: hashGenerator(req.body.password),
-            adress: req.body.adress
-        });
+  if (!user) {
+    console.log(req.body);
+    const newUser = new User({
+      email: req.body.email,
+      name: req.body.name,
+      password: hashGenerator(req.body.password),
+      adress: req.body.adress
+    });
 
-        newUser.save((err) => {
-        err ? res.status(403).send(err) : res.status(202).json(newUser)
-        })
-      }
-      else {
-        res.json({msg: "Email already taken"});
-      }
-
+    newUser.save((err) => {
+      err ? res.status(403).send(err) : res.status(202).json(newUser)
+    })
+  }
+  else {
+    res.json({ msg: "Email already taken" });
+  }
 }
 
 const auth = async (req, res, next) => {
@@ -44,8 +44,8 @@ const auth = async (req, res, next) => {
       const copyUser = await User.findOne(user).select(['-password']);
 
       res.status(202).json({
-          token: token, 
-          user: copyUser
+        token: token,
+        user: copyUser
       });
     } else {
       res.status(403).json({ msg: "Login failed. Invalid credentials." });
@@ -59,7 +59,7 @@ const products = async (req, res, next) => {
   if (token === tokenInCookies) {
     const userPayload = verifyToken(token);
 
-    if (userPayload.role == 'admin') {
+    if (userPayload.role == admin) {
       const newProduct = new Product({
         title: req.body.title,
         price: req.body.price,
@@ -69,7 +69,7 @@ const products = async (req, res, next) => {
         serial: req.body.serial
       })
       newProduct.save((err) => {
-        err ? res.status(403).send(err) : res.status(202).json({newProduct: newProduct});
+        err ? res.status(403).send(err) : res.status(202).json({ newProduct: newProduct });
       });
     } else {
       res.status(403).json({ msg: 'Unauthorized' });
@@ -82,31 +82,83 @@ const products = async (req, res, next) => {
 const orders = async (req, res, next) => {
 
   const token = req.cookies["auth-token"];
-
   if (token === tokenInCookies) {
     const userPayload = verifyToken(token);
 
     const postOrder = new Order({
-        status: req.body.status,
-        items: req.body.items,
-        buyer: userPayload.userId,
-        orderValue: req.body.items.length
+      status: req.body.status,
+      items: req.body.items,
+      buyer: userPayload.userId,
+      orderValue: req.body.items.length
     });
     const orderValue = postOrder.items;
-    let total =[];
+    let total = [];
     orderValue.forEach(element => {
-        total.push(element.price)
+      total.push(element.price)
     });
     console.log(total)
     console.log(orderValue)
     postOrder.save((err) => {
-        if (err) console.error(err);
-        res.status(202).json(postOrder);
+      if (err) console.error(err);
+      res.status(202).json(postOrder);
     });
 
-    } else {
-        res.status(403).json({msg: "Please, log in"});
-    }
+  } else {
+    res.status(403).json({ msg: "Please, log in" });
+  }
 
 };
-module.exports = { register, auth, products, orders }
+
+const allProducts = (req, res, next) => {
+  const products = await Product.find({})
+  res.json(products)
+}
+
+const productById = (req, res, next) => {
+  const product = await Product.findById(req.params.id)
+  res.json(product)
+}
+
+const removeProduct = (req, res, next) => {
+  const token = req.cookies["auth-token"];
+  if (token === tokenInCookies) {
+    const userPayload = verifyToken(token);
+    if (userPayload.role == admin) {
+      const removeProduct = await Product.deleteOne({ _id: req.params.id });
+      res.json(removeProduct);
+    } else {
+      res.json({ msg: 'Unauthorized' })
+    }
+  } else {
+    res.json({ msg: "Please, log in" });
+  }
+}
+
+const updateProduct = (res, req, next) => {
+  const token = req.cookies["auth-token"];
+
+  if (token === tokenInCookies) {
+    const userPayload = verifyToken(token);
+    if (userPayload.role == admin) {
+      const updateProduct = await Product.updateOne({ _id: req.params.id }, { $set: { price: req.body.price } })
+      res.json(updateProduct);
+    } else {
+      res.json({ msg: 'Unauthorized' })
+    }
+  } else {
+    res.json({ msg: "Please, log in" });
+  }
+}
+
+
+const allOrders = (req, res, next) => {
+  const token = req.cookies["auth-token"];
+  if (token === tokenInCookies) {
+    const userPayload = verifyToken(token);
+    const orders = (userPayload.role == admin) ? await Order.find({}) : await Order.find({ buyer: userPayload.userId });
+    res.json(orders);
+  } else {
+    res.json({ msg: "Please, log in" });
+  }
+}
+module.exports = { register, auth, products, orders, allProducts, allOrders, productById, removeProduct }
